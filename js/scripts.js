4a67dd4bef0e882a79aa1fd32a32fe7da86f994e
Ram Zallan <Ram.Zallan@hthla.org>
2016-03-15 14:53:19 -0700

    Created initial site.

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+
+    $('.counter').each(function () {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+
+        $({
+            countNum: $this.text()
+        }).animate({
+                countNum: countTo
+            },
+
+            {
+
+                duration: 2500,
+                easing: 'linear',
+                step: function () {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function () {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                }
+
+            });
+
+
+
+    });
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 500);
+                    return false;
+                }
+            }
+        });
+    });
+});
