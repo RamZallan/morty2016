40c612174823d6432386efe297c1877a046f37e3
Ram Zallan <Ram.Zallan@hthla.org>
2016-03-15 15:26:26 -0700

    Added text and image to About section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $(".button-collapse").sideNav();
     $('.parallax').parallax();
+    $('.materialboxed').materialbox();
 
     $('.counter').each(function () {
         var $this = $(this),
